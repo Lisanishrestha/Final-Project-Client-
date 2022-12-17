eff7c3e1da5079d5cce887fd495fc99d6ba3a737
Lisu .s <lisu.s@Lisus-MBP.fios-router.home>
2022-12-17 16:24:14 -0500

    Changed the format

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -4,14 +4,73 @@ AllStudentsView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the all students view page.
 ================================================== */
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles( () => ({
+  image:{  
+    width: '100px',
+    height: '100px'
+  },
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    fontType: 'bold',
+    fontFamily: 'Monospace', 
+    fontSize: '35px', 
+    color: 'darkblue'
+  },
+  appBar:{
+    backgroundColor: '#fcb6bb',
+    shadows: ['none'],
+  },
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#b0c4de',
+    width: "50%",
+    margin: "auto",
+  },
+  links:{
+    textDecoration: 'none',
+  }
+}));
 
 const AllStudentsView = (props) => {
+  const classes = useStyles();
   const {students, deleteStudent} = props;
   // If there is no student, display a message
   if (!students.length) {
     return (
-    <div>
+    <div className={classes.root}>
+    <AppBar position="static" elevation={0} className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" className={classes.title} color="inherit" >
+          Student Finder
+        </Typography>
+
+        <Link className={classes.links} to={'/campuses'} >
+          <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+            All Campuses
+          </Button>
+        </Link>
+
+        <Link className={classes.links} to={'/home'} >
+          <Button variant="contained" color="primary">
+            Home
+          </Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+    
+    <div className={classes.greeting}><h1>All Students</h1></div>
       <p>There are no students.</p>
       <Link to={`newstudent`}>
         <button>Add New Student</button>
@@ -22,7 +81,27 @@ const AllStudentsView = (props) => {
   
   // If there is at least one student, render All Students view 
   return (
-    <div>
+    <div className={classes.root}>
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit" >
+            Student Finder
+          </Typography>
+
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              All Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/'} >
+            <Button variant="contained" color="primary">
+              Home
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+
       <h1>All Students</h1>
 
       {students.map((student) => {
