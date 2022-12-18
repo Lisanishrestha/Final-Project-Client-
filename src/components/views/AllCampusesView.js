5a33b1d5b4dd803c7a1185356b78bfd5762717d3
Jay Liang <57160302+jayliang1234@users.noreply.github.com>
2022-12-17 20:04:49 -0500

    fixed issue

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -117,7 +117,7 @@ const AllCampusesView = (props) => {
         </div>
       ))}
       <br/>
-      <Link to={`/New Campus`}>
+      <Link to={`/newcampus`}>
         <button>Add New Campus</button> 
       </Link>
     </div>
